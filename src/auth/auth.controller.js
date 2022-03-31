6827e1f439120e6f2ace11e88290a11239985c53
DESKTOP-1U4HJ0I\jorge <jkokeito@gmail.com>
2022-03-31 13:02:54 +0200

    make it better

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -26,7 +26,7 @@ export const registerCtrl = async (req, res) => {
             await createPost(req.body.email)//creo en al usuario en la bdd de posts
             // paso 4
             //ojo que el host es el de nuestra aplicación de react
-            sendValidationEmail(req.body.email, `http://localhost:3000/validate?token=${token}`)
+            sendValidationEmail(req.body.email, `https://veryguaysite.netlify.app/validate?token=${token}`)
             res.sendStatus(201);
         } else {
             // mando un 409(conflict) porque ya existe el usuario en BBDD
